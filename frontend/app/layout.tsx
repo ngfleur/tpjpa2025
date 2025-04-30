305becee4a20e9012019871f38462da733a55635
Fleur N’GUESSAN <ngfleur01@gmail.com>
2025-04-30 20:05:06 +0200

    Ajout de la fonctionnalité d'inscription avec gestion des états et messages d'erreur dans le front et back

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,30 +1,34 @@
 import './globals.css';
 import Footer from '@app/components/Layout/Footer';
 import Header from '@app/components/Layout/Header';
-import { ReactNode } from 'react';
-import { Toast, UIProvider } from '@app/components/Provider/UIContext';
-import { LoginModal } from '@app/components/Modal/LoginModal';
+import {ReactNode} from 'react';
+import {UIProvider} from '@app/components/Provider/UIContext';
+import {LoginModal} from '@app/components/Modal/LoginModal';
 import {RegistrationModal} from "@components/Modal/RegistrationModal";
+import {Toaster} from "sonner";
 
 interface LayoutProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
-export default function RootLayout({ children }: LayoutProps) {
-  return (
-    <html>
-    <body>
-    <UIProvider>
-      <div className="min-h-screen flex flex-col pt-16">
-        <Header />
-        <main className="flex-1">{children}</main>
-        <LoginModal />
-        <RegistrationModal />
-        <Footer />
-        <Toast />
-      </div>
-    </UIProvider>
-    </body>
-    </html>
-  );
+export default function RootLayout({children}: LayoutProps) {
+    return (
+        <html>
+        <body>
+        <UIProvider>
+            <div className="min-h-screen flex flex-col pt-16">
+                <Header/>
+                <main className="flex-1">{children}</main>
+                <LoginModal/>
+                <RegistrationModal/>
+                <Footer/>
+                <Toaster
+                    richColors
+                    position="top-left"
+                />
+            </div>
+        </UIProvider>
+        </body>
+        </html>
+    );
 }
